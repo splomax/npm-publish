94e853e56ad0aa5f6bdb903a27db9b720544afde
Sean Lomax <sean.lomax@objectpartners.com>
2020-02-21 12:42:29 -0600

    will this work? I don't know.

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import {FC} from "react";
+export const MyComponent: FC<any> = (props:any) => {
+    return (
+        <React.Fragment>
+            <p>Hello Lib!</p>
+        </React.Fragment>
+    );
+}
